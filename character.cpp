37e3f63e4f3527d92fc6a1793d7f993d78de16f6
theTPH <33541639+theTPH@users.noreply.github.com>
2020-02-21 12:40:45 +0100

    startet lvl up system implementation
    added xp and xp needed for next lvl + getters and setter

--- character.cpp
+++ character.cpp
@@ -5,12 +5,17 @@ Character::Character()
 {
     Attributes attr = Attributes(0, 0, 0);
     this->attributes = attr;
+    this->name = "";
+    this->xp = 0;
+    this->xp_lvl_up = 0;
 }
 
 Character::Character(Attributes attr, string name)
 {
     this->attributes = attr;
     this->name = name;
+    this->xp = 0;
+    this->xp_lvl_up = 0;
 }
 
 
@@ -29,3 +34,23 @@ string Character::get_name()
 {
     return this->name;
 }
+
+void Character::set_xp(int xp)
+ {
+     this->xp = xp;
+ }
+
+ int Character::get_xp()
+ {
+     return this->xp;
+ }
+
+ void Character::set_xp_lvl_up(int xp)
+ {
+     this->xp_lvl_up = xp;
+ }
+
+ int Character::get_xp_lvl_up()
+ {
+     return this->xp_lvl_up;
+ }
--- character.hpp
+++ character.hpp
@@ -6,12 +6,22 @@ class Character
 {
     private:
         Attributes attributes = Attributes(0, 0, 0);
-        string name = "";    
+        string name;    
+        int xp;
+        int xp_lvl_up;
     public:
+    //constructors
     Character();
     Character(Attributes, string);
+    //methods
+    void level_up_system();
     // get and set
     void set_attributes(Attributes);
     Attributes get_attributes();
     string get_name();
+    void set_xp(int);
+    int get_xp();
+    void set_xp_lvl_up(int);
+    int get_xp_lvl_up();
+
 };
